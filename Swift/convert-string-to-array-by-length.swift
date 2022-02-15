90ba45b4487bec8a8da34f96e292b267bb4f38a4
newoo <newoo4297@naver.com>
2022-02-15 13:15:04 +0900

    Add how to convert string to array by length

---
+++ Swift/convert-string-to-array-by-length.swift
@@ -0,0 +1,12 @@
+// https://stackoverflow.com/questions/32212220/how-to-split-a-string-into-substrings-of-equal-length
+
+extension String {
+    func components(withMaxLength length: Int) -> [String] {
+        return stride(from: 0, to: self.count, by: length).map {
+            let start = self.index(self.startIndex, offsetBy: $0)
+            let end = self.index(start, offsetBy: length, limitedBy: self.endIndex) ?? self.endIndex
+            return String(self[start..<end])
+        }
+    }
+}
+
